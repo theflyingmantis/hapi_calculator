1e5d1e141e36bb9e500ac9517fc7734b5872a30e
admin <admin@Abhinav-Rai.local>
2017-05-26 16:59:34 +0530

    Made create calculator POST api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var Calculator = require('./src/calculator');
+const pool = require('./lib/db');
 
 const Hapi = require('hapi');
 
@@ -9,34 +10,68 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({ 
     host: 'localhost', 
-    port: 8000 
+    port: 3000
 });
+
+//*************** HANDLERS ****************************
+var newCalculatorHandler = function(request,reply){
+  var calculatorId = Number(request.payload.id);
+  var initValue = Number(request.payload.val);
+  if(isNaN(initValue)) {
+    initValue=0; 
+  }
+  if(isNaN(calculatorId)){
+    reply({"Status":"Error","Explanation":"Bad ID given"}).code(400);  
+  }
+  //console.log(calculatorId,initValue);
+  pool.query("insert into calculator (id,val) values ($1,$2);",[calculatorId,initValue],function(err,res){
+    if(err != null) {
+      console.error('error running query', err);
+      reply({"Status":"Error","Explanation":err}).code(400);
+    }
+    else{
+      reply({"Status":"Created","Explanation":"Inserted "+initValue+" in calculator id "+calculatorId }).code(201);
+    }
+  });
+};
+
+
+var 
+//**************************************************
 
 // Add the route
 server.register(require('inert'), (err) => {
-
-    if (err) {
-        throw err;
+  if (err) {
+      throw err;
+  }
+  server.route({
+    method: 'GET',
+    path: '/about',
+    handler: function (request, reply) {
+      reply.file('./public/about.html');
     }
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: function (request, reply) {
-            // console.log(request.params);
-            reply.file('./public/index.html');
-        }
-    });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply.file('./public/index.html');
+    }
+  });
 
-    server.route({
-        method: ['GET','POST','PUT'],
-        path: '/newCalculator',
-        handler: function(request,reply){
-            console.log(request.query); 
-            console.log(request.payload);
-            return reply(request.query);
-        }
-    });
+  server.route({
+    method: ['POST'],
+    path: '/newCalculator',
+    handler: newCalculatorHandler
+  });
+
+  server.route({
+    method: ['GET'],
+    path: '/calculatorValue',
+    handler: calculatorValueHandler
+  });
+
 });
 
 // Start the server
@@ -45,5 +80,6 @@ server.start((err) => {
     if (err) {
         throw err;
     }
+    
     console.log('Server running at:', server.info.uri);
 });
